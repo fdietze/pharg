4bb19a615e5b79bc31e78a6684aad15d2426927d
Felix Dietze <felix@beerleader.net>
2016-10-09 18:16:11 +0200

    reorganize packages

--- src/main/scala/GeneralizedHyperGraph.scala
+++ src/main/scala/GeneralizedHyperGraph.scala
@@ -4,118 +4,108 @@ import shapeless._
 import shapeless.syntax._
 import cats._
 
-// requirements:
-// directed graph
-// undirected graph
-// with data on vertices / edges
-// with specific vertex types
-// with specific edges types, connecting specific vertex types
-// edges connecting other edges or vertices
-// hypergraph
-// directed vs undirected hypergraphs
-// - Undirected Hyperedge: (incident:Set[V])
-// - Directed Hyperedge: (in:Set[V], out:Set[V])
-// - Directed Hyperedge: (incident:Seq[V], i: Interpretation[ich])
-//
-//
+import algorithm.Neighbours
 
-package generic {
-  case class Edge[A](in: A, out: A)
-  case class HyperEdge[A](incident: A*)
-
+package directed {
+  trait EdgeLike[A] {
+    def in: A
+    def out: A
+  }
+  case class Edge[A](in: A, out: A) extends EdgeLike[A]
   case class Graph[A](vertices: Set[A], edges: Set[Edge[A]])
-  case class HyperGraph[A](vertices: Set[A], edges: Set[HyperEdge[A]])
-  case class GeneralizedHyperGraph[A](incidents: Map[A, Set[A]])
 
-  trait DirectedEdge[E, A] {
-    def in(edge: E): A
-    def out(edge: E): A
-  }
-
-  trait DirectedHyperEdge[E, A] {
-    def incidents(edge: E): Seq[A]
-  }
-
-  case class TypedGraph[A, E](vertices: Seq[A], edges: Seq[E])(implicit e: DirectedEdge[E, A])
-  case class GeneralizedTypedHyperGraph[A, M[K, V]](atoms: Set[A], incidents: HMap[M])
-
-  case class GeneralizedGraph[A](atoms: Set[A])
-
-  trait Neighbours[G, A] {
-    def neighbours(graph: G, atom: A): Set[A]
-  }
 }
 
-package object generic {
+package object directed {
+  implicit def graphNeighbours[A]: Neighbours[Graph[A], A] = new Neighbours[Graph[A], A] {
+    def neighbours(graph: Graph[A], atom: A) = graph.edges.filter(_.in == atom).map(_.out) ++ graph.edges.filter(_.out == atom).map(_.in)
 
-  def degree[G, A](graph: G, atom: A)(implicit n: Neighbours[G, A]): Int = n.neighbours(graph, atom).size
-  // def map[G, A](graph: G, f: (A) => A)(implicit n: Neighbours[G, A]): Int = (graph.vertices map f), graph.edges map (_ map f)
-  // def filter[G, A](graph: G, atom: (A) => Boolean)(implicit n: Neighbours[G, A]): Int = n.neighbours(graph, atom).size
+  }
 
-  implicit val directedGraphFunctor: Functor[Graph] = new Functor[Graph] {
+  implicit val edgeFunctor: Functor[Edge] = new Functor[Edge] {
+    def map[A, B](fa: Edge[A])(f: A => B) = fa.copy(in = f(fa.in), out = f(fa.out))
+  }
+
+  implicit val graphFunctor: Functor[Graph] = new Functor[Graph] {
     def map[A, B](fa: Graph[A])(f: A => B) = fa.copy(
       vertices = fa.vertices map f,
       edges = fa.edges map (Functor[Edge].map(_)(f))
     )
   }
 
-  implicit val directedEdgeFunctor: Functor[Edge] = new Functor[Edge] {
-    def map[A, B](fa: Edge[A])(f: A => B) = fa.copy(in = f(fa.in), out = f(fa.out))
+}
+
+package directed {
+  package hyper {
+    case class Edge[A](incidents: A*)
+    case class Graph[A](vertices: Set[A], edges: Set[Edge[A]])
   }
 
+  package object hyper {
+
+    implicit def graphNeighbours[A]: Neighbours[Graph[A], A] = new Neighbours[Graph[A], A] {
+      def neighbours(graph: Graph[A], atom: A) = graph.edges.filter(_.incidents contains atom).flatMap(_.incidents) - atom
+    }
+
+  }
+}
+
+package generalized.directed {
+  package hyper {
+    case class Graph[A](incidents: Map[A, Set[A]])
+  }
+  package object hyper {
+
+    implicit def graphNeighbours[A]: Neighbours[Graph[A], A] = new Neighbours[Graph[A], A] {
+      def neighbours(graph: Graph[A], atom: A) = (graph.incidents.values.filter(_ contains atom).flatten.toSet - atom)
+    }
+  }
+}
+
+package typed {
+  package directed {
+    case class Graph[A, E](vertices: Set[A], edges: Set[E])
+  }
+  package object directed {
+    implicit def graphNeighbours[A, E]: Neighbours[Graph[A, E], A] = new Neighbours[Graph[A, E], A] {
+      def neighbours(graph: Graph[A, E], atom: A): Iterable[A] = {
+        graph.edges.collect {
+          case e: pharg.directed.EdgeLike[A] if e.in == atom => e.out
+          case e: pharg.directed.EdgeLike[A] if e.out == atom => e.in
+        }
+      }
+    }
+  }
+}
+
+package generalized {
+  case class Graph[A](atoms: Set[A])
+}
+package object generalized {
   implicit def graphNeighbours[A]: Neighbours[Graph[A], A] = new Neighbours[Graph[A], A] {
-    def neighbours(graph: Graph[A], atom: A) = graph.edges.filter(_.in == atom).map(_.out) ++ graph.edges.filter(_.out == atom).map(_.in)
+    def neighbours(graph: Graph[A], atom: A): Iterable[A] = {
+      graph.atoms.collect {
+        case e: pharg.directed.EdgeLike[A] if e.in == atom => e.out
+        case e: pharg.directed.EdgeLike[A] if e.out == atom => e.in
+      }
+    }
   }
+}
 
-  implicit def hyperGraphNeighbours[A]: Neighbours[HyperGraph[A], A] = new Neighbours[HyperGraph[A], A] {
-    def neighbours(graph: HyperGraph[A], atom: A) = graph.edges.filter(_.incident contains atom).flatMap(_.incident) - atom
+package algorithm {
+  trait Neighbours[G, A] {
+    def neighbours(graph: G, atom: A): Iterable[A]
   }
+}
 
-  implicit def typedGraphNeighbours[A, E](implicit e: DirectedEdge[E, A]): Neighbours[TypedGraph[A, E], A] = new Neighbours[TypedGraph[A, E], A] {
-    def neighbours(graph: TypedGraph[A, E], atom: A) = (
-      graph.edges.filter(e.in(_) == atom).map(e.out(_)) ++
-      graph.edges.filter(e.out(_) == atom).map(e.in(_))
-    ).toSet
-  }
+package object algorithm {
 
-  implicit def generalizedHyperGraphNeighbours[A]: Neighbours[GeneralizedHyperGraph[A], A] = new Neighbours[GeneralizedHyperGraph[A], A] {
-    def neighbours(graph: GeneralizedHyperGraph[A], atom: A) = (graph.incidents.values.filter(_ contains atom).flatten.toSet - atom)
-  }
+  def degree[G, A](graph: G, atom: A)(implicit n: Neighbours[G, A]): Int = n.neighbours(graph, atom).size
+  // def map[G, A](graph: G, f: (A) => A)(implicit n: Neighbours[G, A]): Int = (graph.vertices map f), graph.edges map (_ map f)
+  // def filter[G, A](graph: G, atom: (A) => Boolean)(implicit n: Neighbours[G, A]): Int = n.neighbours(graph, atom).size
 
   // implicit def generalizedTypedHyperGraphNeighbours[A, V <: HList, M[K,V]](implicit ev: M[A,V]): Neighbours[GeneralizedTypedHyperGraph[A, M], A] = new Neighbours[GeneralizedTypedHyperGraph[A, M], A] {
   //   def neighbours(graph: GeneralizedTypedHyperGraph[A, M], atom: A) = graph.atoms.filter(a => graph.incidents.get(a).get.filter[A] contains atom)
-  // }
-
-  // case class Graph(vertices: Set[Vertex], edges: Set[Edge])
-
-  // type Vertex = Atom[HNil]
-  // type DirectedEdge = Atom[Vertex :: Vertex :: HNil]
-  // type HyperEdge = Atom[Vertex :: Vertex :: HNil] // TODO: arbitrary sized list?
-  // type DirectedGraph = GeneralizedHyperGraph[Vertex :+: DirectedEdge :+: CNil]
-
-  // val g: DirectedGraph = GeneralizedHyperGraph(Seq(Atom(HNil), Atom(Atom(HNil), Atom(HNil))))
-
-  // case class Vertex(neighbours: Iterable[Vertex] = Nil)
-
-  // implicit val undirectedVertexNeighbourhood = new Neighbourhood[Vertex] {
-  //   def neighbours(atom: Vertex) = atom.neighbours
-  // }
-
-  // case class DirectedGraph[Vertex: Neighbourhood](vertices: Set[Vertex])
-
-  // val v1 = Vertex()
-  // val v2 = Vertex()
-  // val g = DirectedGraph(Set(Vertex()))
-
-  // case class GeneralizedHyperGraph(atoms: Set[Atom])
-
-  // case class Atom[L <: HList](incidents: L) // Type Class
-  // case class GeneralizedHyperGraph[C <: Coproduct](atoms: Seq[C] = Nil) //Functor, Monoid
-
-  // case class HyperGraph[A, B](vertices: Set[Atom[A]], edges: Seq[Atom[B]]) // Functor, Monoid
-
-  // trait Node[A] {
-  //   def neighbours: Seq[A]
   // }
 
   object SchemaDSL {
--- src/test/scala/GenericGraphSpec.scala
+++ src/test/scala/GenericGraphSpec.scala
@@ -1,37 +1,30 @@
-package pharg.generic
+package pharg
 
-import shapeless._
-import pharg.generic._
+import pharg.algorithm._
 import cats._
 
+// requirements:
+// directed graph
+// undirected graph
+// with data on vertices / edges
+// with specific vertex types
+// with specific edges types, connecting specific vertex types
+// edges connecting other edges or vertices
+// hypergraph
+// directed vs undirected hypergraphs
+// - Undirected Hyperedge: (incident:Set[V])
+// - Directed Hyperedge: (in:Set[V], out:Set[V])
+// - Directed Hyperedge: (incident:Seq[V], i: Interpretation[ich])
+
 class GenericGraphSpec extends org.specs2.mutable.Specification {
-  "Typed Directed Graph" >> {
-    "degree" >> {
-      sealed trait MyTypedVertex
-      case class Post(id: Int) extends MyTypedVertex
-      case class Author(id: Int) extends MyTypedVertex
+  "directed.Graph" >> {
+    import pharg.directed._
 
-      sealed trait MyTypedEdge { def in: MyTypedVertex; def out: MyTypedVertex }
-      case class Writes(in: Author, out: Post) extends MyTypedEdge
-
-      implicit def myEdge: DirectedEdge[MyTypedEdge, MyTypedVertex] = new DirectedEdge[MyTypedEdge, MyTypedVertex] {
-        def in(e: MyTypedEdge) = e.in
-        def out(e: MyTypedEdge) = e.out
-      }
-
-      val tg = TypedGraph(
-        Seq[MyTypedVertex](Post(1), Post(2), Author(1)),
-        Seq[MyTypedEdge](Writes(Author(1), Post(1)), Writes(Author(1), Post(2)))
-      )
-      degree(tg, Author(1): MyTypedVertex) mustEqual 2
-    }
-  }
-
-  "Directed Graph" >> {
     val g = Graph(
       Set(1, 2),
       Set(Edge(1, 2))
     )
+
     "degree" >> {
       degree(g, 1) mustEqual 1
     }
@@ -41,62 +34,59 @@ class GenericGraphSpec extends org.specs2.mutable.Specification {
     }
   }
 
-  "Hypergraph" >> {
-    val hg = HyperGraph(Set(1, 2, 3), Set(HyperEdge(1, 2, 3), HyperEdge(2, 3)))
-    degree(hg, 2) mustEqual 2
+  "directed.hyper.Graph" >> {
+    import pharg.directed.hyper._
+
+    val g = Graph(Set(1, 2, 3), Set(Edge(1, 2, 3), Edge(2, 3)))
+
+    "degree" >> {
+      degree(g, 2) mustEqual 2
+    }
   }
 
-  "Generalized Hypergraph" >> {
-    val hg = GeneralizedHyperGraph(Map(2 -> Set(1, 3), 4 -> Set(1, 2)))
-    degree(hg, 2) mustEqual 1
-    degree(hg, 1) mustEqual 2
-    degree(hg, 4) mustEqual 0
+  "generalized.directed.hyper.Graph" >> {
+    import pharg.generalized.directed.hyper._
+
+    val g = Graph(Map(2 -> Set(1, 3), 4 -> Set(1, 2)))
+
+    "degree" >> {
+      degree(g, 2) mustEqual 1
+      degree(g, 1) mustEqual 2
+      degree(g, 4) mustEqual 0
+    }
   }
 
-  // "Generalized Typed Hypergraph" >> {
-  //   sealed trait MyAtom
-  //   case class Author(name: String) extends MyAtom
-  //   case class Writes(date: Long) extends MyAtom
-  //   case class Post(title: String) extends MyAtom
-  //   case class RespondsTo(date: Long) extends MyAtom
+  "Typed Directed Graph" >> {
+    import typed.directed._
 
-  //   class Allowed[K, V]
-  //   implicit val allowAtom = new Allowed[MyAtom, HNil]
-  //   implicit val allowWrites = new Allowed[Writes, Author :: Post :: HNil]
-  //   implicit val allowRespondsToPost = new Allowed[RespondsTo, Post :: Post :: HNil]
-  //   implicit val allowRespondsToRespondsTo = new Allowed[RespondsTo, Post :: RespondsTo :: HNil]
+    sealed trait MyTypedVertex
+    case class Post(id: Int) extends MyTypedVertex
+    case class Author(id: Int) extends MyTypedVertex
 
-  //   val author = Author("Uwe")
-  //   val writes = Writes(0)
-  //   val post1 = Post("Engel")
-  //   val post2 = Post("Wust")
-  //   val post3 = Post("Ultrakrass")
-  //   val respondsTo1 = RespondsTo(1)
-  //   val respondsTo2 = RespondsTo(2)
+    sealed trait MyTypedEdge extends directed.EdgeLike[MyTypedVertex] { def in: MyTypedVertex; def out: MyTypedVertex }
+    case class Writes(in: Author, out: Post) extends MyTypedEdge
 
-  //   val g = GeneralizedTypedHyperGraph[MyAtom, Allowed](
-  //     Set(author, writes, post1, post2, post3, respondsTo1, respondsTo2),
-  //     HMap[Allowed](
-  //       writes -> (author :: post1 :: HNil),
-  //       respondsTo1 -> (post1 :: post2 :: HNil),
-  //       respondsTo2 -> (post3 :: respondsTo1 :: HNil)
-  //     )
-  //   )
+    val g = Graph[MyTypedVertex, MyTypedEdge](
+      Set(Post(1), Post(2), Author(1)),
+      Set(Writes(Author(1), Post(1)), Writes(Author(1), Post(2)))
+    )
 
-  //   // implicit def myAtom: DirectedHyperEdge[MyTypedEdge, MyTypedVertex] = new DirectedEdge[MyTypedEdge, MyTypedVertex] {
-  //   //   def in(e: MyTypedEdge) = e.in
-  //   //   def out(e: MyTypedEdge) = e.out
-  //   // }
+    "degree" >> {
+      degree(g, Author(1): MyTypedVertex) mustEqual 2
+    }
 
-  //   degree(g, author) mustEqual 1
-  //   degree(g, post1) mustEqual 2
-  //   degree(g, respondsTo1) mustEqual 1
-  //   degree(g, respondsTo2) mustEqual 0
-  // }
+    // "map" >> {
+    //   Functor[Graph].map(g) {
+    //     case Post(id) => Post(id + 1)
+    //     case Author(id) => Author(id * 2)
+    //   } mustEqual Graph(Set(2, 3), Set(Edge(2, 3)))
+    // }
+  }
 
-  "Generalized Typed Hypergraph" >> {
+  "Typed Generalized Hypergraph" >> {
+    import generalized._
     sealed trait MyAtom
-    sealed trait MyEdge extends MyAtom { def in: MyAtom; def out: MyAtom }
+    sealed trait MyEdge extends MyAtom with pharg.directed.EdgeLike[MyAtom]
     case class Author(name: String) extends MyAtom
     case class Writes(in: Author, out: Post, date: Long) extends MyEdge
 
@@ -112,26 +102,21 @@ class GenericGraphSpec extends org.specs2.mutable.Specification {
     val respondsTo1 = RespondsTo(post1, post2, 1)
     val respondsTo2 = RespondsTo(post3, respondsTo1, 2)
 
-    val g = GeneralizedGraph[MyAtom](Set(author, writes, post1, post2, post3, respondsTo1, respondsTo2))
-
-    implicit val generalizedGraphNeighbours: Neighbours[GeneralizedGraph[MyAtom], MyAtom] = new Neighbours[GeneralizedGraph[MyAtom], MyAtom] {
-      def neighbours(graph: GeneralizedGraph[MyAtom], atom: MyAtom): Set[MyAtom] = {
-        graph.atoms.collect {
-          case e: MyEdge if e.in == atom => e.out
-          case e: MyEdge if e.out == atom => e.in
-        }.toSet
-      }
-    }
+    val g = Graph[MyAtom](Set(author, post1, post2, post3, writes, respondsTo1, respondsTo2))
 
     "degree" >> {
-      degree[GeneralizedGraph[MyAtom], MyAtom](g, author) mustEqual 1
-      degree[GeneralizedGraph[MyAtom], MyAtom](g, post1) mustEqual 2
-      degree[GeneralizedGraph[MyAtom], MyAtom](g, respondsTo1) mustEqual 1
-      degree[GeneralizedGraph[MyAtom], MyAtom](g, respondsTo2) mustEqual 0
+      degree[Graph[MyAtom], MyAtom](g, author) mustEqual 1
+      degree[Graph[MyAtom], MyAtom](g, post1) mustEqual 2
+      degree[Graph[MyAtom], MyAtom](g, respondsTo1) mustEqual 1
+      degree[Graph[MyAtom], MyAtom](g, respondsTo2) mustEqual 0
     }
 
     // "map" >> {
-    //   Functor[GeneralizedGraph].map(g)({ case Post(title) => Post(title.toUpperCase) }.applyOrElse(_, identity)) mustEqual Graph(Set(2, 3), Set(Edge(2, 3)))
+    //   Functor[Graph].map(g)(
+    //     {
+    //       case Post(title) => Post(title.toUpperCase)
+    //     }.applyOrElse(_, identity)
+    //   ) mustEqual Graph(Set(2, 3), Set(Edge(2, 3)))
     // }
   }
 }
